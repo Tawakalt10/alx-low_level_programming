8ea6166bdf0c64d02cf8e470f078bfd0b12a123b
Tawakalt10 <raheemtawa@gmail.com>
2023-08-29 03:41:32 -0700

    all

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,7 +16,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listin_t;
+} listint_t;
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
